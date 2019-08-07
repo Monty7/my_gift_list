8cfabf9f1ed38e73ac0f1bbb9b46bedc2a255a02
github username <github email address>
2019-08-07 14:03:54 -0500

    added nested routes for items under categories with views and controllers

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,14 @@
 class ItemsController < ApplicationController
+
+    def index
+   
+        #/categories/:category_id/items
+        #/items
+        #/how to handle whether a user wants to see all items /index or nested items /categories/:category_id/index
+        @category = Category.find(params[:category_id])
+       # @category = Category.find(params[:id])
+        @items = @category.items
+     
+
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  resources :personal_lists
+  resources :lists
   resources :list_items
-  resources :items
-  resources :categories
+  #resources :items
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :categories, only: [:index] do
+    resources :items, only: [:index]
+  end
+  # get '/categories' => 'categories#index'
+  # get '/categories/:id/items/:item_id' =>  'items#index'
 end
